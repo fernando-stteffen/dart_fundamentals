fb6d57d2e87db02e60a7ac622e8b354e892ec3fc
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-01 19:10:23 -0300

    feat: adding some variable types

---
+++ lib/1_variables/variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  int age = 37;
+  double priceHour = 12.000;
+  String name = 'Mohaabdu';
+  // num is a father for int and double
+  num alternativeAge = 38;
+  num bonusPriceHour = 10.000;
+
+  var discoveryMyType = 10;
+
+  Object anyObject = 1;
+  dynamic anyDynamic = 1;
+
+  anyDynamic = 'test';
+
+  bool myBool = true;
+
+}
