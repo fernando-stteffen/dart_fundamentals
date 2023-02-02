ef1cfdb081e5352ad7e9f1123d83f3b0d2583634
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-02 13:20:15 -0300

    add: conditional operators examples

---
+++ lib/3_conditions_operators/3_conditions_operators.dart
@@ -0,0 +1,30 @@
+void main() {
+  final age = 16;
+
+  if (age > 16) {
+    print('Can be have an license');
+  } else {
+    print('Can\'t be have an license');
+  }
+
+  if (age >= 16) {
+    print('Can be have an license');
+  } else {
+    print('Can\'t be have an license');
+  }
+
+  if (age > 16) {
+    print('Can be have an license');
+  } else if ( age == 16) {
+    print('Maybe Can be have an license');
+  } else {
+    print('Can\'t be have an license');
+  }
+
+  //equality ==
+  //difference !=
+  //> big then
+  //< small then
+  // >= big then or equal
+  // <= small then or equal
+}
