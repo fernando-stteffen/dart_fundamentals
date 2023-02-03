25ca7f5dfaec5afcf2412273d5f6f46515606d7c
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-02 21:19:04 -0300

    feat: add switchs examples

---
+++ lib/3_conditions_operators/4_switchs.dart
@@ -0,0 +1,30 @@
+void main() {
+  final weekDay = 9;
+
+  switch (weekDay) {
+    case 0:
+      print("Monday");
+      break;
+    case 1:
+      print("Tuesday");
+      break;
+    case 2:
+      print("Wednesday");
+      break;
+    case 3:
+      print("Thursday");
+      break;
+    case 4:
+      print("Friday");
+      break;
+    case 5:
+      print("Saturday");
+      break;
+    case 6:
+      print("Sunday");
+      break;
+    default:
+      print("NoDay!");
+      break;
+  }
+}
