1b49fc78ee34e85074ceec04a959fcf32e0c4b30
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-04 15:25:01 -0300

    feat: add lists examples

---
+++ lib/5_lists/1_lists.dart
@@ -0,0 +1,17 @@
+void main() {
+  //commun declaration
+  List<int> carsNumbers = [1, 2, 3];
+
+  //seek to use this
+  var listNumbers = [1, 2, 3];
+
+  List<int> listWithoutData = [];
+
+  var listWithoutData1 = []; // that is dynamic type
+  var listWithoutData2 = <int>[];
+
+  var listNames = ['Steve', 'Bill', 'Richard'];
+  var listNamesWithoutData = <String>[];
+
+  
+}
