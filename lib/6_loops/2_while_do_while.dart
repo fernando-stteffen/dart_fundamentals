5355c7c3c467565ab6fc84094b78346343293444
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-06 22:06:42 -0300

    feat: add loops while and do while examples

---
+++ lib/6_loops/2_while_do_while.dart
@@ -0,0 +1,45 @@
+void main() {
+  var number = 10;
+  // var numberToExtract = 450;
+  // var rest = 0;
+
+  // var totalBills = [0, 0, 0, 0, 0, 0, 0];
+  // var bills = [
+  //   'One houndred dollars',
+  //   'Fifty dollars',
+  //   'Twenty dollars',
+  //   'Ten dollars',
+  //   'Five dollars',
+  //   'One dollar'
+  // ];
+
+  // var oneHoundredBills = numberToExtract / 100;
+  // totalBills[0] = int.parse(((oneHoundredBills.toString()).split('.'))[0]);
+
+  // var fiftyBills =
+  //     (int.parse(((oneHoundredBills.toString()).split('.'))[1]) * 100) / 50;
+  // totalBills[1] = int.parse(((fiftyBills.toString()).split('.'))[0]);
+
+  // var twentyBills =
+  //     (int.parse(((fiftyBills.toString()).split('.'))[1]) * 50) / 20;
+  // totalBills[2] = int.parse(((twentyBills.toString()).split('.'))[0]);
+
+  // var tenBills =
+  //     (int.parse(((twentyBills.toString()).split('.'))[1]) * 20) / 10;
+  // totalBills[3] = int.parse(((twentyBills.toString()).split('.'))[0]);
+
+  // var fiveBills = (int.parse(((tenBills.toString()).split('.'))[1]) * 10) / 5;
+  // totalBills[4] = int.parse(((twentyBills.toString()).split('.'))[0]);
+
+  // var oneBill = (int.parse(((fiveBills.toString()).split('.'))[1]) * 5);
+  // totalBills[5] = oneBill;
+
+  // while (number < totalBills.length) {
+  //   print('${bills[number]} ${totalBills[number]} ok');
+  //   number++;
+  // }
+
+  do {
+    print('this is the most uneusefull loop of all times');
+  } while (number > 100);
+}
